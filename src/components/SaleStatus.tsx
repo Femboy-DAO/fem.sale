022de29f09cf965aa81de0740b32a42c6ec2fb13
Tomás Thor Palmeirim <0xexplorer@protonmail.com>
2022-04-02 18:12:10 +0100

    added countdown timer

--- src/components/SaleStatus.tsx
+++ src/components/SaleStatus.tsx
@@ -50,9 +50,33 @@ export const SaleStatus = () => {
     }
   </>
 
+  var countDownDate = new Date("Apr 8, 2022 00:00:00").getTime();
+  var x = setInterval(function() {
+    var now = new Date().getTime();
+
+    var distance = countDownDate - now;
+
+    let countDownTimer = document.getElementById("countdown")!
+
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    countDownTimer.innerHTML = days + "d " + hours + "h "
+    + minutes + "m " + seconds + "s ";
+
+    if (distance < 0) {
+      clearInterval(x);
+      countDownTimer.innerHTML = "Let the femboy takeover commence.";
+    }
+  }, 1000);
+
   return (
+    
     <div className='container-content'>
-      <h1>Sale Info</h1>
+      <h1>Coming soon uWu</h1>
+      <h5 id="countdown"></h5>
       <p><b>Start:</b> <StartsAt /></p>
       <p><b>End:</b> <EndsAt /></p>
       <p><b>Spend Deadline:</b> <DeadlineAt /></p>
