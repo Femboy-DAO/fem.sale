22085a720f7ee4f2808aef9c5b2e54a4c93c74a3
0xfem <0xfem@protonmail.com>
2022-04-07 20:34:23 -0600

    add socials footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,34 @@
+import { ReactNode } from "react"
+import { DISCORD_URL, GITHUB_URL, TWITTER_URL } from "../config/links"
+import Twitter from "../images/twitter.png";
+import Github from "../images/github.png";
+import Discord from "../images/discord.png";
+
+const LinkExt = ({ href, children }: { href: string; children: ReactNode; }) =>
+  <a rel="noopener noreferrer" target="_blank" href={href}>
+  {children}
+</a>
+/* {{
+  backgroundColor: "#F8F8F8",
+  borderTop: "1px solid #E7E7E7",
+  textAlign: "center",
+  padding: "20px",
+  position: "fixed",
+  left: "0",
+  bottom: "0",
+  height: "60px",
+  width: "100%"
+}} */
+
+export const Footer = () => {
+  return <div className="footer-container">
+    <div className="footer">
+
+    <LinkExt href={TWITTER_URL}><img className="fa" src={Twitter} alt="Twitter logo" /></LinkExt>
+
+<LinkExt href={DISCORD_URL}><img className="fa" src={Discord} alt="Discord logo" /></LinkExt>
+
+<LinkExt href={GITHUB_URL}><img className="fa" src={Github} alt="Github logo" /></LinkExt>
+    </div>
+</div>
+}
---
+++ src/config/links.ts
@@ -0,0 +1,3 @@
+export const DISCORD_URL = 'https://discord.gg/MFRSxppn8D';
+export const GITHUB_URL = 'https://github.com/Femboy-DAO';
+export const TWITTER_URL = 'https://twitter.com/FemboyDAO';
